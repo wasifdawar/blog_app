ae46d80c228a40c4eb8fbb1de6a7d7951aff699b
wasifdawar <m.wasifdawar@gmail.com>
2021-11-28 03:23:51 +0500

    Creating Blog App using React Redux Thunk

---
+++ src/components/PostList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {fetchPosts} from '../actions'
+
+class PostList extends React.Component{
+    componentDidMount() {
+        console.log(this.props.fetchPosts())
+    }
+    render() {
+        return (<div className="ui container">
+            Post List
+        </div>)
+    }
+}
+
+export default connect(null,{fetchPosts:fetchPosts})(PostList);
